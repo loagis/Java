5c1672c93313813472083ad965d12e080f700f27
loagis <sebastiankohlhepp95@gmail.com>
2018-11-06 13:48:04 +0100

    changes

--- Challange/StringCompressor.java
+++ Challange/StringCompressor.java
@@ -1,35 +1,45 @@
-@Service
+//@Service
 public class StringCompressor {
     
-    public String compress(String uncompressed) {
+    public static String compress(String uncompressed) {
         int len = uncompressed.length();
-        char ch;
-        char lastCh;
-        int count = 1;
-        String compressed;
+        char ch = 0;
+        char lastCh = 0;
+        int count = 0;
+        String compressed = "";
 
-        for(int i=0;i<=len;i++){
+        for(int i=0;i<len;i++){
+            System.out.println(uncompressed.charAt(i));
             ch = uncompressed.charAt(i);
-            if(lastCh==uncompressed.charAt(i)){
-                ++count;
+            if(lastCh==ch){
+                count++;
                 if(count>=2){
                     
-                    for(;i<=len;++i){
-                        if(lastCh==uncompressed.charAt(i)){
-                           ++count;
+                    for(;i<len;++i){
+                        System.out.println("for schleife");
+                        ch = uncompressed.charAt(i);
+                        if(lastCh==ch){
+                           count++;
                        }else{
-                        compressed += count;
-                        --i;
+                            System.out.println("else block");
+                            compressed = compressed+String.valueOf(count);
+                            --i;
+                            count = 0;
+                            break;
                        }
                     }
 
-                }else{compressed += (String)ch;}
+                }else{compressed = compressed+String.valueOf(ch);}
             }else{
-                compressed += (String)ch;
-                lastCh = uncompressed.charAt(i);
+                count = 0;
+                compressed = compressed+String.valueOf(ch);
+                lastCh = ch;
             }
         }
-
-        return compressed;
+        System.out.println(compressed+String.valueOf(count));
+        return compressed+String.valueOf(count);
+    }
+    public static void main(String[] args){
+        System.out.println("wort ist "+compress("aabbbbcddddddd"));
     }
 }
